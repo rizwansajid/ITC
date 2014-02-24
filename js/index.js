fa825a476cbdf48768271a2cab3ca1b32a2ee5fb
Arslan <aabhatti76@gmail.com>
2014-02-24 17:51:27 +0500

    fjalskdf

--- js/index.js
+++ js/index.js
@@ -14,13 +14,13 @@ function registerPushwooshIOS(email) {
         //navigator.notification.alert(JSON.stringify(notification));
 
         //reset badges on icon
-        pushNotification.setApplicationIconBadgeNumber(0);
+        pushNotification.setApplicationIconBadgeNumber(2);
     });
 
     pushNotification.registerDevice({ alert: true, badge: true, sound: true, pw_appid: "3913D-FBE0C", appname: "ITC" },
 									function (status) {
 									    var deviceToken = status['deviceToken'];
-									    alert('registerDevice: ' + deviceToken);
+									    //alert('registerDevice: ' + deviceToken);
 									    InsertPushToken(email, deviceToken);
 									    onPushwooshiOSInitialized(deviceToken);
 									},
@@ -44,10 +44,10 @@ function onPushwooshiOSInitialized(pushToken) {
 							 });
 
     //start geo tracking. PWTrackSignificantLocationChanges - Uses GPS in foreground, Cell Triangulation in background. 
-    pushNotification.startLocationTracking('PWTrackSignificantLocationChanges',
-									function () {
-									    console.warn('Location Tracking Started');
-									});
+   // pushNotification.startLocationTracking('PWTrackSignificantLocationChanges',
+	//								function () {
+	//								    console.warn('Location Tracking Started');
+	//								});
 }
 
 
@@ -62,7 +62,7 @@ function InsertPushToken(email, deviceToken) {
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (response) {
-            alert("Details Updated Successfully!");
+            //alert("Details Updated Successfully!");
         },
         failure: function (errMsg) {
             alert("Something Went Wrong!");
@@ -70,121 +70,10 @@ function InsertPushToken(email, deviceToken) {
     });
 }
 
-//function registerPushwooshAndroid() {
 
-//    var pushNotification = window.plugins.pushNotification;
-
-//    //push notifications handler
-//    document.addEventListener('push-notification', function (event) {
-//        var title = event.notification.title;
-//        var userData = event.notification.userdata;
-
-//        //dump custom data to the console if it exists
-//        if (typeof (userData) != "undefined") {
-//            console.warn('user data: ' + JSON.stringify(userData));
-//        }
-
-//        //and show alert
-//        navigator.notification.alert(title);
-
-//        //stopping geopushes
-//        pushNotification.stopGeoPushes();
-//    });
-
-//    //projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID"
-//    pushNotification.registerDevice({ projectid: "60756016005", appid: "4F0C807E51EC77.93591449" },
-//									function (token) {
-//									    alert(token);
-//									    //callback when pushwoosh is ready
-//									    onPushwooshAndroidInitialized(token);
-//									},
-//									function (status) {
-//									    alert("failed to register: " + status);
-//									    console.warn(JSON.stringify(['failed to register ', status]));
-//									});
-//}
-
-//function onPushwooshAndroidInitialized(pushToken) {
-//    //output the token to the console
-//    console.warn('push token: ' + pushToken);
-
-//    var pushNotification = window.plugins.pushNotification;
-
-//    pushNotification.getTags(function (tags) {
-//        console.warn('tags for the device: ' + JSON.stringify(tags));
-//    },
-//						 function (error) {
-//						     console.warn('get tags error: ' + JSON.stringify(error));
-//						 });
-
-
-//    //set multi notificaiton mode
-//    //pushNotification.setMultiNotificationMode();
-//    //pushNotification.setEnableLED(true);
-
-//    //set single notification mode
-//    //pushNotification.setSingleNotificationMode();
-
-//    //disable sound and vibration
-//    //pushNotification.setSoundType(1);
-//    //pushNotification.setVibrateType(1);
-
-//    pushNotification.setLightScreenOnNotification(false);
-
-//    //goal with count
-//    //pushNotification.sendGoalAchieved({goal:'purchase', count:3});
-
-//    //goal with no count
-//    //pushNotification.sendGoalAchieved({goal:'registration'});
-
-//    //setting list tags
-//    //pushNotification.setTags({"MyTag":["hello", "world"]});
-
-//    //settings tags
-//    pushNotification.setTags({ deviceName: "hello", deviceId: 10 },
-//									function (status) {
-//									    console.warn('setTags success');
-//									},
-//									function (status) {
-//									    console.warn('setTags failed');
-//									});
-
-//    function geolocationSuccess(position) {
-//        pushNotification.sendLocation({ lat: position.coords.latitude, lon: position.coords.longitude },
-//								 function (status) {
-//								     console.warn('sendLocation success');
-//								 },
-//								 function (status) {
-//								     console.warn('sendLocation failed');
-//								 });
-//    };
-
-//    // onError Callback receives a PositionError object
-//    //
-//    function geolocationError(error) {
-//        alert('code: ' + error.code + '\n' +
-//			  'message: ' + error.message + '\n');
-//    }
-
-//    function getCurrentPosition() {
-//        navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
-//    }
-
-//    //greedy method to get user position every 3 second. works well for demo.
-//    //	setInterval(getCurrentPosition, 3000);
-
-//    //this method just gives the position once
-//    //	navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
-
-//    //this method should track the user position as per Phonegap docs.
-//    //	navigator.geolocation.watchPosition(geolocationSuccess, geolocationError, { maximumAge: 3000, enableHighAccuracy: true });
-
-//    //Pushwoosh Android specific method that cares for the battery
-//    pushNotification.startGeoPushes();
-//}
 
 function initPushwoosh() {
-    alert("initpushwoosh");
+   // alert("initpushwoosh");
     var pushNotification = window.plugins.pushNotification;
     //if (device.platform == "Android") {
     //    registerPushwooshAndroid();
@@ -194,7 +83,7 @@ function initPushwoosh() {
     if (device.platform == "iPhone" || device.platform == "iOS") {
         registerPushwooshIOS(email);
         pushNotification.onDeviceReady();
-        alert("initPW success");
+       // alert("initPW success");
     }
 }
 
